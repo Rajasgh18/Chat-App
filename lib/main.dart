5461f79b421aea0a6dc9debcb7dacb0b852b7ad9
Raja Singh <rajasgh18@gmail.com>
2024-04-24 21:31:16 +0530

    changes to UI layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -15,57 +16,65 @@ class MyApp extends StatelessWidget {
       title: 'Chat App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFF6a5bc2),
-        fontFamily: GoogleFonts.poppins().fontFamily,
+        scaffoldBackgroundColor: const Color(0xFF6a5bff),
+        fontFamily: GoogleFonts.nunito().fontFamily,
         useMaterial3: true,
       ),
       home: Scaffold(
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(140),
           child: AppBar(
-            flexibleSpace: const Padding(
-              padding: EdgeInsets.all(12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IntrinsicHeight(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Hi Raja!",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Colors.white70,
+            flexibleSpace: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const IntrinsicHeight(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Hi, Raja!",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Colors.white70,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "You Received",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
+                          SizedBox(height: 2),
+                          Text(
+                            "You Received",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "48 Messages",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
+                          SizedBox(height: 4),
+                          Text(
+                            "48 Messages",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  Icon(
-                    Icons.menu_rounded,
-                    size: 32,
-                    color: Colors.white,
-                  )
-                ],
+                    ColorFiltered(
+                      colorFilter: const ColorFilter.mode(
+                        Colors.white, // Desired color
+                        BlendMode.srcIn,
+                      ),
+                      child: SvgPicture.asset(
+                        'assets/icons/menu.svg', // Path to your SVG file
+                        width: 32,
+                        height: 32,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             backgroundColor: Colors.transparent,
@@ -77,6 +86,56 @@ class MyApp extends StatelessWidget {
               borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(40), topRight: Radius.circular(40)),
               color: Colors.white,
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ColorFiltered(
+                        colorFilter: const ColorFilter.mode(
+                            Color(0xFF6a5bff), BlendMode.srcIn),
+                        child: SvgPicture.asset(
+                          'assets/icons/search.svg',
+                          height: 24,
+                          width: 24,
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                Colors.yellow.shade300),
+                            foregroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                            elevation: MaterialStateProperty.all<double>(0),
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 4))),
+                        child: Row(
+                          children: [
+                            const Text("Direct Messages"),
+                            const SizedBox(width: 8),
+                            Container(
+                              padding: const EdgeInsets.all(6),
+                              decoration: const BoxDecoration(
+                                  color: Colors.black, shape: BoxShape.circle),
+                              child: const Text(
+                                "4",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 12),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      TextButton(onPressed: () {}, child: const Text("Group 4"))
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
