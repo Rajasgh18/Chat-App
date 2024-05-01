9ba57684a445f6c5fd9eb86eeeb9f6db638e2917
Raja Singh <rajasgh18@gmail.com>
2024-05-01 21:42:12 +0530

    Server setup

---
+++ lib/screens/chat.dart
@@ -0,0 +1,23 @@
+import 'package:chat_app/components/box_wrapper.dart';
+import 'package:flutter/material.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({super.key});
+
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const BoxWrapper(
+        child: Column(
+          children: [Text("Hello")],
+        ),
+      ),
+    );
+  }
+}
