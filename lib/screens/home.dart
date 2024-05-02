879a144dc4e4b6d2ea0e33c2ab19ff2d680751bb
Raja Singh <rajasgh18@gmail.com>
2024-05-02 23:25:37 +0530

    fixed ui issue

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -35,96 +35,99 @@ class Home extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 12),
-        BoxWrapper(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ColorFiltered(
-                      colorFilter: const ColorFilter.mode(
-                          Color(0xFF6a5bff), BlendMode.srcIn),
-                      child: SvgPicture.asset(
-                        'assets/icons/search.svg',
-                        height: 24,
-                        width: 24,
+        Expanded(
+          flex: 1,
+          child: BoxWrapper(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ColorFiltered(
+                        colorFilter: const ColorFilter.mode(
+                            Color(0xFF6a5bff), BlendMode.srcIn),
+                        child: SvgPicture.asset(
+                          'assets/icons/search.svg',
+                          height: 24,
+                          width: 24,
+                        ),
                       ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.yellow.shade300),
-                          foregroundColor:
-                              MaterialStateProperty.all(Colors.black),
-                          elevation: MaterialStateProperty.all<double>(0),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.symmetric(
-                                  horizontal: 16, vertical: 4))),
-                      child: Row(
-                        children: [
-                          const Text("Direct Messages"),
-                          const SizedBox(width: 8),
-                          Container(
-                            padding: const EdgeInsets.all(6),
-                            decoration: const BoxDecoration(
-                                color: Colors.black, shape: BoxShape.circle),
-                            child: const Text(
-                              "4",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 12),
-                            ),
-                          )
-                        ],
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                Colors.yellow.shade300),
+                            foregroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                            elevation: MaterialStateProperty.all<double>(0),
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 4))),
+                        child: Row(
+                          children: [
+                            const Text("Direct Messages"),
+                            const SizedBox(width: 8),
+                            Container(
+                              padding: const EdgeInsets.all(6),
+                              decoration: const BoxDecoration(
+                                  color: Colors.black, shape: BoxShape.circle),
+                              child: const Text(
+                                "4",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 12),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    TextButton(onPressed: () {}, child: const Text("Group 4"))
-                  ],
+                      TextButton(onPressed: () {}, child: const Text("Group 4"))
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  "Pinned Messages(2)",
-                  style: TextStyle(fontSize: 16, color: Colors.black87),
+                const SizedBox(height: 16),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "Pinned Messages(2)",
+                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 4),
-              const ChatItem(
-                image: "assets/avatar1.jpg",
-                status: "active",
-                name: "Goku",
-                message:
-                    "Hey, It's been a while since we've talked. Let's meet",
-              ),
-              const ChatItem(
-                image: "assets/avatar2.jpg",
-                status: "snooze",
-                name: "Satoru Gojo",
-                message:
-                    "Hey, It's been a while since we've talked. Let's meet",
-              ),
-              const SizedBox(height: 12),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  "All Messages(8)",
-                  style: TextStyle(fontSize: 16, color: Colors.black87),
+                const SizedBox(height: 4),
+                const ChatItem(
+                  image: "assets/avatar1.jpg",
+                  status: "active",
+                  name: "Goku",
+                  message:
+                      "Hey, It's been a while since we've talked. Let's meet",
                 ),
-              ),
-              const SizedBox(height: 4),
-              const ChatItem(
-                image: "assets/avatar3.jpg",
-                status: "active",
-                name: "Yamanaka",
-                message:
-                    "Hey, It's been a while since we've talked. Let's meet",
-              ),
-            ],
+                const ChatItem(
+                  image: "assets/avatar2.jpg",
+                  status: "snooze",
+                  name: "Satoru Gojo",
+                  message:
+                      "Hey, It's been a while since we've talked. Let's meet",
+                ),
+                const SizedBox(height: 12),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "All Messages(8)",
+                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
+                ),
+                const SizedBox(height: 4),
+                const ChatItem(
+                  image: "assets/avatar3.jpg",
+                  status: "active",
+                  name: "Yamanaka",
+                  message:
+                      "Hey, It's been a while since we've talked. Let's meet",
+                ),
+              ],
+            ),
           ),
         ),
       ],
