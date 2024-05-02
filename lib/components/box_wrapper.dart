7671ae327bf272da7b5765eb2e25f8ebb587b8ea
Raja Singh <rajasgh18@gmail.com>
2024-05-02 23:25:21 +0530

    fixed layout issue

--- lib/components/box_wrapper.dart
+++ lib/components/box_wrapper.dart
@@ -7,17 +7,15 @@ class BoxWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(32), topRight: Radius.circular(32)),
-          color: Colors.white,
-        ),
-        child: child,
+    return Container(
+      // width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(32), topRight: Radius.circular(32)),
+        color: Colors.white,
       ),
+      child: child,
     );
   }
 }
