9e20134cdc20bce78ab86f6cc34a3b2a1ab83363
Raja Singh <rajasgh18@gmail.com>
2024-05-03 20:25:39 +0530

    removed some imports

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,7 @@
 import "package:chat_app/components/box_wrapper.dart";
 import "package:chat_app/components/chat_item.dart";
 import "package:chat_app/components/contact_avatar.dart";
-import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
-import "package:flutter/widgets.dart";
 import "package:flutter_svg/flutter_svg.dart";
 
 class Home extends StatelessWidget {
