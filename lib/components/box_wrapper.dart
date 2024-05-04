0e327cad4b4317d4ac151c3a5451d12cf30c83f5
Raja Singh <rajasgh18@gmail.com>
2024-05-04 23:01:32 +0530

    UI changes

--- lib/components/box_wrapper.dart
+++ lib/components/box_wrapper.dart
@@ -8,12 +8,12 @@ class BoxWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // width: MediaQuery.of(context).size.width,
+      width: MediaQuery.of(context).size.width,
       padding: const EdgeInsets.symmetric(vertical: 16),
       decoration: const BoxDecoration(
         borderRadius: BorderRadius.only(
             topLeft: Radius.circular(32), topRight: Radius.circular(32)),
-        color: Colors.white,
+        color: Color(0xFFefefef),
       ),
       child: child,
     );
